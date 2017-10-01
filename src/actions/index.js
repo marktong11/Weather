afd13e4d8617f602e0fa7cacd7cb03cc88d2ae40
Mark Tong <marktong11@gmail.com>
2017-10-01 00:30:31 -0400

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+const API_KEY = 'f6f2d46efb6aae0267ad4e85eb956a37';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATER } from '../actions/index'
+
+export default function(state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			// return state.concat([action.payload.data]);
+			return [ action.payload.data, ...state ];
+	}
+
+	return state;
+}
